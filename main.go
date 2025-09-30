626c6da6a8dc03c2897f46d8a44e1840db5bd872
skullzado <pafcalzado@gmail.com>
2025-09-30 14:09:11 +0800

    Add get all chirps func

--- main.go
+++ main.go
@@ -51,7 +51,9 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
