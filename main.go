ba52c517bc57b38a2248310b2d5c1764b3c538f5
skullzado <pafcalzado@gmail.com>
2025-10-01 12:55:38 +0800

    Add update users func

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
